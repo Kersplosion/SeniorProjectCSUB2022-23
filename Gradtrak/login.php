ee503357dad68a52139202cdba14640adb263af2
JustiinU <114719550+JustiinU@users.noreply.github.com>
2023-02-23 23:12:29 -0800

    Update login.php

--- Gradtrak/login.php
+++ Gradtrak/login.php
@@ -14,13 +14,17 @@
     <body>
         <div id ="login">
             <form action = "<?= $_SERVER["PHP_SELF"] ?>" method="POST">
-                <h2> LOG IN </h2>
-                    <label> Net ID </label>
-                    <input type = "text" name = "username" placeholder = "Net ID"><br>
-                    <label> Password </label>
-                    <input type = "password" name = "password" placeholder = "Password"><br>
-                    <input type = "submit" value = "Log in">
-                </form>
-            </div>
-        </body>
-    </html>
+                <div class = "h2">                    
+                    <h2> LOG IN </h2>
+                </div>
+                <label> Net ID </label>
+                <input type = "text" name = "username" placeholder = "Net ID"><br>
+                <br>
+                <label> Password </label>
+                <input type = "password" name = "password" placeholder = "Password"><br>
+                <br>
+                <input type = "submit" value = "Log in">
+            </form>
+        </div>
+    </body>
+</html>
