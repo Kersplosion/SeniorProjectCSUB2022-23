71cbdb392eeac8c5e2366790d183e7599553bd77
JustiinU <114719550+JustiinU@users.noreply.github.com>
2023-04-03 16:39:53 -0700

    added <br> tag

--- Gradtrak/student.php
+++ Gradtrak/student.php
@@ -86,6 +86,7 @@
   </table>
 </div>
 <br>
+<br>
 <!--Accordion that displays student course history -->
 <!-- Source: https://www.w3schools.com/bootstrap/bootstrap_collapse.asp -->
 <!-- General Education Requirements -->
